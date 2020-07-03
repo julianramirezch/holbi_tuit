214f14f81f14622652e1f9fda10e276d6f4bd643
julianramirezch <julianramirezch1@gmail.com>
2020-07-03 11:35:21 -0500

    [add]: holbi tuit 2.0

--- holbi_tuit.py
+++ holbi_tuit.py
@@ -14,29 +14,79 @@ def tuit_decision(dec):
 
 def get_news_link():
     """ Get First news link """
-    r = requests.get("https://news.ycombinator.com/best")
-    webpage = html.fromstring(r.content)
-    links = webpage.xpath('//a/@href')
-    if len(links[11]) <= 140:
-        print('1. {}'.format(links[11]))
-    if len(links[17]) <= 140:
-        print('2. {}'.format(links[17]))
-    if len(links[23]) <= 140:
-        print('3. {}'.format(links[23]))
+    print('1. HackerNews')
+    print('2. The Verge')
+    user_portal = input('Select your portal (1, 2): ')
+    while not user_portal.isnumeric():
+        user_portal = input('Select your portal (1, 2): ')
 
-    user_selection_link = input('Select your link (1, 2, 3,): ')
-    while not user_selection_link.isnumeric():
+    # HackerNews Selection
+    if int(user_portal) == 1:
+        r = requests.get("https://news.ycombinator.com/best")
+        webpage = html.fromstring(r.content)
+        links = webpage.xpath('//a/@href')
+        if len(links[11]) <= 115:
+            print('1. {}'.format(links[11]))
+        if len(links[17]) <= 115:
+            print('2. {}'.format(links[17]))
+        if len(links[23]) <= 115:
+            print('3. {}'.format(links[23]))
+
         user_selection_link = input('Select your link (1, 2, 3,): ')
+        while not user_selection_link.isnumeric():
+            user_selection_link = input('Select your link (1, 2, 3,): ')
 
-    if int(user_selection_link) == 1:
-        return(str(links[11]))
-    elif int(user_selection_link) == 2:
-        return(str(links[17]))
-    elif int(user_selection_link) == 3:
-        return(str(links[23]))
-    else:
-        print('ERROR: Choose 1, 2 or 3')
-        return('')
+        if int(user_selection_link) == 1:
+            return(str(links[11]))
+        elif int(user_selection_link) == 2:
+            return(str(links[17]))
+        elif int(user_selection_link) == 3:
+            return(str(links[23]))
+        else:
+            print('ERROR: Choose 1, 2 or 3')
+            return('')
+
+    # The Verge Selection
+    if int(user_portal) == 2:
+        r = requests.get("https://www.theverge.com/")
+        webpage = html.fromstring(r.content)
+        links = webpage.xpath('//a/@href')
+        if len(links[86]) <= 115:
+            print('1. {}'.format(links[86]))
+        if len(links[89]) <= 115:
+            print('2. {}'.format(links[89]))
+        if len(links[92]) <= 115:
+            print('3. {}'.format(links[92]))
+        if len(links[95]) <= 115:
+            print('4. {}'.format(links[95]))
+        if len(links[98]) <= 115:
+            print('5. {}'.format(links[98]))
+        if len(links[101]) <= 115:
+            print('6. {}'.format(links[101]))
+        if len(links[104]) <= 115:
+            print('7. {}'.format(links[104]))
+
+        user_selection_link = input('Select your link (1, 2, 3, 4, 5, 6, 7): ')
+        while not user_selection_link.isnumeric():
+            user_selection_link = input('Select your link (1, 2, 3,): ')
+
+        if int(user_selection_link) == 1:
+            return(str(links[86]))
+        elif int(user_selection_link) == 2:
+            return(str(links[89]))
+        elif int(user_selection_link) == 3:
+            return(str(links[92]))
+        elif int(user_selection_link) == 4:
+            return(str(links[95]))
+        elif int(user_selection_link) == 5:
+            return(str(links[98]))
+        elif int(user_selection_link) == 6:
+            return(str(links[101]))
+        elif int(user_selection_link) == 7:
+            return(str(links[104]))
+        else:
+            print('ERROR: Choose 1, 2 or 3')
+            return('')
 
 
 def holbi_tuit():
@@ -46,7 +96,7 @@ def holbi_tuit():
     access_token = 'YOUR ACCESS TOKEN'
     access_token_secret = 'YOUR ACCESS TOKEN SECRET'
     link = get_news_link()
-    message = 'Hi, this is Holbi tuit \U0001F916 ! I was created to tweet every day. I use the news portal https://news.ycombinator.com/ and this is the top news: '
+    message = 'Hi, this is Holbi tuit \U0001F916 ! I was created to tweet every day. I use the news portal https://news.ycombinator.com/ or https://www.theverge.com/ this is the top news: '
     tuit = message + '\n' + link
     if len(tuit) <= 280:
         print('Today Tuit:')
